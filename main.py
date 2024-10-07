be34fa3a2dad694efb9b97f30e951ece0887f200
Dishita Sharma <dsharma1@mines.edu>
2024-10-07 16:47:03 -0600

    fixed issue with adding more than one violation to each cell

--- main.py
+++ main.py
@@ -2,12 +2,9 @@
 #AlgoBowl main.py
 
 
-#ISSUE - THE REASON WE ARE PRINTING A BUNCH OF LIGHTBULBS IS BECAUSE WE ARENT RETURNING OUR GRID 
-# AND WE DONT PASS BY REFERENCE OR BY POINTER SO GRID IS NEVER UPDATED - FIX 
-
 def checkCoverage(grid):
     check = True
-    for row in range(len(grid)+1):
+    for row in range(len(grid)):
         for col in range(len(grid[row])+1):
             # Check left (move left along the row)
             for c in range(col - 1, -1, -1):
@@ -51,8 +48,8 @@ def checkCoverage(grid):
 
 def totalViolations(grid):
     total = 0
-    for row in range(len(grid)+1):
-        for col in range(len(grid[row])+1):
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
             if isinstance(grid[row][col], int) and grid[row][col] != -1:  # Checking if it's a light bulb (indicated by int)
                 temp = lightBulbViolations(grid, (row, col))
                 total += temp
@@ -77,7 +74,7 @@ def lightBulbViolations(grid, position):
         
 
     # Check right (move right along the row)
-    for c in range(col + 1, len(grid[row])+1):
+    for c in range(col + 1, len(grid[row])):
         if isinstance(grid[row][c], int)  and grid[row][c] != -1:  # Found another light bulb
             violationCount += 1
         elif not grid[row][c] == -1 and grid[row][c].startswith("G"):  # Stop if you hit a grey cell
@@ -143,8 +140,8 @@ def findHighestViolation(grid):
     highest_violation = 0
     highest_position = None
 
-    for row in range(len(grid)+1):
-        for col in range(len(grid[row])+1):
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
             if isinstance(grid[row][col], int) and grid[row][col] != -1:  # Only consider cells where a light bulb can be placed
                 violations = lightBulbViolations(grid, (row, col)) #+ greyCellViolations(grid, (row, col))
                 if violations > highest_violation:
@@ -184,7 +181,6 @@ def main():
                 addThis.append("G")  # Gray cell
         grid.append(addThis)
     
-   
     # Loop through, remove violations, check coverage, repeat
     
     # Count violations
@@ -208,26 +204,34 @@ def main():
                 grid = potentialNewGrid
                 curr_violation_count = new_violation_count
 
+    printgrid = grid
+    for row in range(int(rows)):
+        for col in range(int(cols)):
+            if isinstance(printgrid[row][col], int) and  printgrid[row][col] != 0 and printgrid[row][col] != -1:
+                printgrid[row][col] = 1
+    
+    curr_violation_count = totalViolations(printgrid)
+    
 
-
+    
     # Write results to output
     with open('output.txt', 'w') as f:
         f.write(str(curr_violation_count) + '\n')  # Convert the violation count to string
-        for i in range(int(rows)+1):
+        for i in range(int(rows)):
             for j in range(int(cols)):
-                if grid[i][j] == "G0":
+                if printgrid[i][j] == "G0":
                     f.write('0')
-                elif grid[i][j] == "G1":
+                elif printgrid[i][j] == "G1":
                     f.write('1')
-                elif grid[i][j] == "G2":
+                elif printgrid[i][j] == "G2":
                     f.write('2')
-                elif grid[i][j] == "G3":
+                elif printgrid[i][j] == "G3":
                     f.write('3')
                 elif grid[i][j] == "G4":
                     f.write('4')  
-                elif grid[i][j] == "G": 
+                elif printgrid[i][j] == "G": 
                     f.write('X')  # Gray cell
-                elif grid[i][j] == -1 :  # Blank cell
+                elif printgrid[i][j] == -1 :  # Blank cell
                     f.write('.')  
                 else:
                     f.write('L')  # Light bulb
